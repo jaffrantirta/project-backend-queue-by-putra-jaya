201b12ac469cba9cacaaa4137d3087d871137310
jaffrantirta <franartika@gmail>
2021-10-10 08:36:30 +0800

    tinggal order

---
+++ app/Util/StatusUtil.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Util;
+use App\Models\Car_type;
+use App\Models\Queue_number;
+use Carbon\Carbon;
+class StatusUtil {
+    public static function update($shop)
+    {
+        
+    }
+}
